9bfbf93208b71ad0e3932dae5b580ce89df9511b
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-08-16 15:50:13 -0400

    Added querySelector to target the cells, startButton, resetButton, scoreDisplay, gameMessage.

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Connected");
+// Verifying connection with index.html
+console.log("Connected and ready to play!");
+
+// Selection of necessary elements from DOM
+// const cells - selects all the cells
+const cells = document.querySelectorAll(".cell");
+// const startButton - used to connect the Start Button
+const startButton = document.querySelector(".start-button");
+// const resetButton - used to connect the Reset Button
+const resetButton = document.querySelector(".reset-button");
+// const scoreDisplay - used to display the scores for both players
+const scoreDisplay = document.querySelector(".score");
+// const gameMessage - used to display winner message
+const gameMessage = document.querySelector(".game-message");
+
+// Debugging logs
+console.log('We have access to Cells', cells);
+console.log('We have access to start-button', startButton);
+console.log('We have access to reset-button', resetButton);
+console.log('We have access to score', scoreDisplay);
+console.log('We have access to game-message', gameMessage);
+
+
+// Game variables
+
+
+
+// Defining what winning is
+
+
+
+/**Definining classes needed
+ * One for the Player that will be used for X and O
+ * One for the Game to manage how it works.
+ */
+
+
+
+// Add necessary instances 
+
+
+
+// Add Event listeners
